8d77f2101c84bc84979e71d547d54c3ba10bab9e
mattwu24 <matthew.wu@colorado.edu>
2019-12-05 13:36:59 -0700

    My lab 3 script

--- lab3_script.sh
+++ lab3_script.sh
@@ -8,10 +8,30 @@ echo "Enter File Name: "
 read fileName
 echo "Enter a regular expression: "
 read pattern
-grep $pattern $fileName
+
+#Problem 2
+grep -E $pattern $fileName
+
+#Problem 3
 echo "Number of phone numbers:"
-grep -c '^\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' regex_practice.txt
+grep -c '^\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}$' regex_practice.txt
+
+#Problem 4.1
 echo "Number of emails:"
-grep -E -o -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" regex_practice.txt
-grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" regex_practice.txt >> email_results.txt
-grep '^\((303)\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' regex_practice.txt
+grep -E -o -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{3}\b" regex_practice.txt
+#grep -E -o -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]\{3\}\b" regex_practice.txt
+
+#Problem 4.2
+echo "Phone numbers with 303 area code"
+grep -c -o '^\((303)\|303\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}$' regex_practice.txt
+
+#Problem 4.3
+grep -E "\b[A-Za-z0-9._%+-]+@geocities.com" regex_practice.txt >> email_results.txt
+
+#Problem 5
+git add "email_results.txt"
+
+#Problem 6
+git commit -m "Emails from geocities.com"
+
+
