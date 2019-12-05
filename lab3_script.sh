2290854f24055774d09e0c707116922818377021
mattwu24 <matthew.wu@colorado.edu>
2019-12-05 13:52:20 -0700

    Updated lab 3 script

--- lab3_script.sh
+++ lab3_script.sh
@@ -5,33 +5,33 @@
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
 echo "Enter File Name: "
-read fileName
+read fileName	#stores input
 echo "Enter a regular expression: "
-read pattern
+read pattern	#stores input
 
 #Problem 2
-grep -E $pattern $fileName
+grep -E $pattern $fileName	#execute grep using user input
 
 #Problem 3
 echo "Number of phone numbers:"
-grep -c '^\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}$' regex_practice.txt
+grep -c '^\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}$' regex_practice.txt	#call grep using regex for phone numbers and print count
 
 #Problem 4.1
 echo "Number of emails:"
-grep -E -o -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{3}\b" regex_practice.txt
-#grep -E -o -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]\{3\}\b" regex_practice.txt
+grep -E -o -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{3}\b" regex_practice.txt	#call grep using regex for emails and print count
 
 #Problem 4.2
-echo "Phone numbers with 303 area code"
-grep -c -o '^\((303)\|303\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}$' regex_practice.txt
+echo "Phone numbers with 303 area code:"
+grep -o '^\((303)\|303\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}$' regex_practice.txt	#call grep using regex for 303 phone numbers and print results
 
 #Problem 4.3
-grep -E "\b[A-Za-z0-9._%+-]+@geocities.com" regex_practice.txt >> email_results.txt
+grep -E "\b[A-Za-z0-9._%+-]+@geocities.com" regex_practice.txt >> email_results.txt #call grep to find all geocities emails and store in email_results.txt
 
 #Problem 5
-git add "email_results.txt"
+git add "email_results.txt"	#stage file of email results
 
 #Problem 6
-git commit -m "Emails from geocities.com"
+git commit -m "Emails from geocities.com"	#commit file
+echo "test"
 
 
